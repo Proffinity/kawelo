8c31e70121fc918304ac44c61ca7c5f4f1cdd8e3
danielndhlovu <danielndhlovu@gmail.com>
2021-03-22 11:08:49 +0200

    Add files via upload

---
+++ assets/pages/datatables.init.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    $('#datatable, #datatable1').DataTable();
+
+    //Buttons examples
+    var table = $('#datatable-buttons').DataTable({
+        lengthChange: false,
+        buttons: ['copy', 'excel', 'pdf', 'colvis']
+    });
+
+    table.buttons().container()
+        .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
+} );
